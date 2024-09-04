b30f54922ee9dfa78e9486b8295037efdd2facf8
jo-and75 <jaotika@gmail.com>
2024-09-04 17:05:55 +0300

    Add append method to LInkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,12 +1,19 @@
 class LinkedList 
-
-
-end 
+  def append(entry) 
+    if @value == nil 
+      @value = entry  
+      @next_node = entry
+    else 
+      @next_node.next = entry 
+      @next_node = entry
+    end
+  end
+end
 
 class Node 
-  def initialize 
-    @value = nil 
+  attr_accessor :value , :next_node
+  def initialize
+    @value = nil
     @next_node = nil
   end
-
 end
